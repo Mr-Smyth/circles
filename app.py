619b3695bac449a9781eb412bb2829d9a06a3640
Eamonn Smyth <e.smyth75@gmail.com>
2020-11-02 12:31:14 +0000

    Update app.py comments

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-#   SETUP env VARIABLES
+# Setup env variables
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-#   SETUP INSTANCE OF PyMongo
+# Setup instance of PyMongo
 mongo = PyMongo(app)
 
 
@@ -56,7 +56,6 @@ def search():
             return redirect(url_for("home"))
     # return list of results and the error for no results.
     return render_template("home.html", people=people, error=error)
-
 
 
 @app.route("/add_person/", methods=["GET", "POST"])
--- helpers/utils.py
+++ helpers/utils.py
@@ -9,12 +9,12 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-#   SETUP env VARIABLES
+# Setup env variables
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-#   SETUP INSTANCE OF PyMongo
+# Setup instance OF PyMongo
 mongo = PyMongo(app)
 
 
