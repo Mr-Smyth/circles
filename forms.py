4789ee857754716e8e57f10f0ee22b17ecb4d3d4
Eamonn Smyth <e.smyth75@gmail.com>
2021-02-22 21:56:57 +0000

    Add a fields.py to the project to handle registering users

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=2, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 Length(min=6, max=20),
+                                                 EqualTo('password')])
+    register = SubmitField('Register')
