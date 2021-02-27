d829575557112ee1bc1bef3ed8dfa9c59ea8175c
Eamonn Smyth <e.smyth75@gmail.com>
2021-02-27 21:17:57 +0000

    Set debug to False

--- app.py
+++ app.py
@@ -1060,4 +1060,4 @@ def server_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
