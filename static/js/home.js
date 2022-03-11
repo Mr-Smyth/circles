49b9a3de74bd0e551a72faee6c160341a42f24b2
Eamonn Smyth <59707387+Mr-Smyth@users.noreply.github.com>
2022-03-11 22:26:11 +0000

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */ 
 document.addEventListener('DOMContentLoaded', function () {
 
-    /* HERE WE DISPLAY HOME PAGE SEARCH FORM AND HIDE SEARCH BUTTON */
+    /* HERE WE DISPLAY THE HOME PAGE SEARCH FORM AND ALSO HIDE SEARCH BUTTON */
     const searchBtn= document.querySelector(".search-button");
     searchBtn.onclick = function () {
         document.querySelector(".search-button").style.display = "none";
